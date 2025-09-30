5e15f038e7426859a7dafcf1af63cf894531ed8b
lucasmodin <lumo0002@stud.kea.dk>
2025-09-30 21:49:16 +0200

    setup board state with composable

---
+++ src/composables/boardState.ts
@@ -0,0 +1,13 @@
+import type { Card } from '@/domain/card'
+import { ref, computed } from 'vue'
+
+const cards = ref<Card[]>([])
+
+const backlog = computed(() => cards.value.filter(card => card.status === 'backlog'))
+const doing = computed(() => cards.value.filter(card => card.status === 'doing'))
+const review = computed(() => cards.value.filter(card => card.status === 'review'))
+const done = computed(() => cards.value.filter(card => card.status === 'done'))
+
+export function useBoardState() {
+    return { cards, backlog, doing, review, done}
+}
