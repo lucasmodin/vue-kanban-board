84775e08335e0ce1f5f4212aa89cecc569cba391
lucasmodin <lumo0002@stud.kea.dk>
2025-10-01 17:25:40 +0200

    carditem might be done

---
+++ src/CardItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Card} from '@/domain/card'
+
+const props = defineProps<{ card: Card }>()
+
+const emit = defineEmits(['edit', 'delete'])
+
+function onDragStart(e: DragEvent) {
+    e.dataTransfer?.setData('text/plain', props.card.id)
+    if(e.dataTransfer) {
+        e.dataTransfer.effectAllowed = 'move'
+    }
+}
+</script>
+
+<template>
+<v-card 
+outlined
+class="cursor-move"
+draggable="true"
+@dragstart="onDragStart"
+>
+    <v-card-title>{{ card.title }}</v-card-title>
+    <v-card-text>{{ card.description }}</v-card-text>
+    <v-card-actions>
+        <v-btn icon="mdi-pencil" @click="emit('edit')"></v-btn>
+        <v-btn icon="mdi-delete" @click="emit('delete')"></v-btn>
+    </v-card-actions>
+</v-card>
+</template>
